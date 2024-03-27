4a8a50911494854135364bd3efc085cedc8a5b29
dnfheh <dnfheh@naver.com>
2024-03-28 01:10:33 +0900

    dho2

--- src/test/java/com/hodolog/controller/PostControllerTest.java
+++ src/test/java/com/hodolog/controller/PostControllerTest.java
@@ -47,9 +47,9 @@ public class PostControllerTest {
 //                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
 //                .param("title", "글 제목입니다.")
 //                        .param("content", "글 내용입니다 하하")
-//                )
+//                )dho
 //                .andExpect(status().isOk())
-//                .andExpect(content().string("Hello World")) 
+//                .andExpect(content().string("Hello World"))
 //                .andDo(print());
 
         mockMvc.perform(post("/posts") // application/json
