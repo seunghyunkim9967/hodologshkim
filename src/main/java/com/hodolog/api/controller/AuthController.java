c2cb4f17287156d0211daea6428fd4b6c10ccb08
dnfheh <dnfheh@naver.com>
2024-06-10 20:11:19 +0900

    20240610

--- src/main/java/com/hodolog/api/controller/AuthController.java
+++ src/main/java/com/hodolog/api/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.hodolog.api.domain.Users;
 import com.hodolog.api.exception.InvalidRequest;
 import com.hodolog.api.exception.InvalidSigninInformation;
 import com.hodolog.api.request.Login;
+import com.hodolog.api.response.SessionResponse;
 import com.hodolog.api.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,12 +23,12 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/auth/login")
-    public String login(@RequestBody Login login) {
+    public SessionResponse login(@RequestBody Login login) {
         // json 아이디/비밀번호
         log.info(">>>login = {}", login);
         // DB에서 조회
         String accessToken = authService.signin(login);
-        return accessToken;
+        return new SessionResponse(accessToken);
         // 토큰을 응답
 //        return user;
     }
