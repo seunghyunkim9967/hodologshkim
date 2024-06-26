e21f69140e8688f571bfb849cc93cbdcab6e30c4
dnfheh <dnfheh@naver.com>
2024-06-26 21:16:26 +0900

    20240626

--- src/main/java/com/hodolog/api/controller/AuthController.java
+++ src/main/java/com/hodolog/api/controller/AuthController.java
@@ -7,6 +7,9 @@ import com.hodolog.api.exception.InvalidSigninInformation;
 import com.hodolog.api.request.Login;
 import com.hodolog.api.response.SessionResponse;
 import com.hodolog.api.service.AuthService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Key;
 import java.time.Duration;
 import java.util.Optional;
 
@@ -32,19 +36,24 @@ public class AuthController {
     @PostMapping("/auth/login")
     public ResponseEntity<Object> login(@RequestBody Login login) {
         String accessToken = authService.signin(login);
-        ResponseCookie cookie = ResponseCookie.from("SESSION", accessToken)
-                .domain("localhost") // todo 서버 환경에 따른 분리 필요 dev.myservice.com resource 폴더(로컬, 개발, 운영 구분하여 세팅)
-                .path("/")
-                .httpOnly(true)
-                .secure(false)
-                .maxAge(Duration.ofDays(30))
-                .sameSite("Strict")
-                .build();
-        log.info(">>>>>>>>> cookie={}", cookie.toString());
 
-        return ResponseEntity.ok()
-                .header(HttpHeaders.SET_COOKIE, cookie.toString())
-                .build();
+        Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+        String jws = Jwts.builder().setSubject("Joe").signWith(key).compact();
+
+//        ResponseCookie cookie = ResponseCookie.from("SESSION", accessToken)
+//                .domain("localhost") // todo 서버 환경에 따른 분리 필요 dev.myservice.com resource 폴더(로컬, 개발, 운영 구분하여 세팅)
+//                .path("/")
+//                .httpOnly(true)
+//                .secure(false)
+//                .maxAge(Duration.ofDays(30))
+//                .sameSite("Strict")
+//                .build();
+//        log.info(">>>>>>>>> cookie={}", cookie.toString());
+//
+//        return ResponseEntity.ok()
+//                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+//                .build();
     }
 
 //    @PostMapping("/auth/login")
