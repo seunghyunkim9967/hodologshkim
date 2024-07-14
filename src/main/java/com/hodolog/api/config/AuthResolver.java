312a4b251e2307f2cc57cc4d73cd50a62d56cbf8
dnfheh <dnfheh@naver.com>
2024-07-14 12:23:52 +0900

    20240714

--- src/main/java/com/hodolog/api/config/AuthResolver.java
+++ src/main/java/com/hodolog/api/config/AuthResolver.java
@@ -56,6 +56,7 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
                     .build()
                     .parseClaimsJws(String.valueOf(jws));
             log.info(">>>>>", claims);
+            //claims.getBody()
             //OK, we can trust this JWT
         } catch (JwtException e) {
             throw new Unauthorized();
