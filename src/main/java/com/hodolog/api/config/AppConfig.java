630a94a5cdc0b67f614cf115c90b31a130d024e1
dnfheh <dnfheh@naver.com>
2024-07-28 20:08:52 +0900

    20240728

--- src/main/java/com/hodolog/api/config/AppConfig.java
+++ src/main/java/com/hodolog/api/config/AppConfig.java
@@ -1,5 +1,11 @@
 package com.hodolog.api.config;
 
-public class AppConfig {
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
+@ConfigurationProperties(prefix = "hodolman")
+public class AppConfig {
+    //application.yml의
+    // hodolman:
+    //   hello : "world" 가 들어가게 됨.
+    public String hello;
 }
